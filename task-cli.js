60d53787f93a8592b7d78b27beaae6a616f5e416
Josuer <josuer.online.work@gmail.com>
2025-11-25 19:43:15 +0800

    feat: + add initial input line read

--- task-cli.js
+++ task-cli.js
@@ -1,8 +1,32 @@
+const readline = require('readline');
 
-function main() {
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+function waitForUserInput() {
+    return new Promise((resolve) => {
+        rl.question('', (input) => {
+            resolve(input.trim())
+        })
+    })
+}
+
+async function main() {
     try {
+        let userInput = await waitForUserInput()
+        while (userInput !== 'quit') {
+            console.log('This is the user input: ', userInput);
+            userInput = await waitForUserInput()
+        }
 
+        rl.close();
     } catch (error) {
-
+        console.log(`Error: ${error}`)
     }
+}
+
+if (require.main === module) {
+    main();
 }
