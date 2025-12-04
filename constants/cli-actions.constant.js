395c109d1929e129516650136b70296b07769a29
Josuer Bague <josuer.online.work@gmail.com>
2025-12-05 06:19:40 +0800

    feat: + add ability to change existing task status

--- constants/cli-actions.constant.js
+++ constants/cli-actions.constant.js
@@ -1,7 +1,9 @@
 const CLI_ACTIONS = {
     ADD: 'add',
     UPDATE: 'update',
-    DELETE: 'delete'
+    DELETE: 'delete',
+    PROGRESS: 'mark-in-progress',
+    DONE: 'mark-done'
 }
 
 module.exports = CLI_ACTIONS;
